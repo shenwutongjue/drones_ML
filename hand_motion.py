9cd669bde3685e262d7e4f5b32530df5bc2ef1f1
shenwutongjue <1753993743@qq.com>
2024-01-31 13:19:02 -0800

    Add standardization test
    test standardization function for smaller input

--- hand_motion.py
+++ hand_motion.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[22]:
 
 
 import numpy as np
@@ -33,13 +33,13 @@ import matplotlib.pyplot as plt
 import pickle
 
 
-# In[4]:
+# In[23]:
 
 
 get_ipython().system('jupyter notebook --version')
 
 
-# In[5]:
+# In[24]:
 
 
 hand_df = pd.read_csv("data/sensorFile.csv", na_values=['?'])
@@ -65,7 +65,7 @@ X_test = hand_df_test.drop(['Gesture'], axis = 1)
 print(X)
 
 
-# In[66]:
+# In[25]:
 
 
 # data preprocessing
@@ -87,15 +87,29 @@ sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f")
 plt.show()
 
 
-# In[68]:
+# In[ ]:
 
 
+#Testing Standardization for Smaller Inputs
 
-data_point = np.array([14326, 28217, 36235, 31276, 32500])
-X_standardized1 = standard_scaler.fit_transform(data_point)
+hand_df_short = pd.read_csv("data/sensorFile_Michael_test.csv", na_values=['?'])
+hand_df_short = hand_df_short.fillna(0)
+hand_df_short = hand_df_short.drop([0])
 
 
-# In[7]:
+hand_df_short = hand_df_short.drop(['GyroX','GyroY','GyroZ'], axis = 1)
+
+y_test1 = hand_df_short['Gesture']
+X_test1 = hand_df_short.drop(['Gesture'], axis = 1)
+
+print(X_test1)
+
+X_standardized1 = standard_scaler.fit_transform(X_test1)
+
+print("\n",X_standardized1)
+
+
+# In[29]:
 
 
 # Check for datapoint similarity
@@ -116,13 +130,20 @@ sns.pairplot(data_standardized, hue='class', palette='viridis')
 plt.show()
 
 
-# In[33]:
+# In[34]:
 
 
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 best_k = None
 best_accuracy = 0
+
+X_train, X_valid, y_train, y_valid = train_test_split(X_standardized,y, train_size=0.75, random_state = 0)
+
+
+X_test = X_test_standardized
+
+X_test = X_test_standardized
 
 print('\n k\tacc_train\tacc_valid\tacc_test\tMean CVS')
 
@@ -131,7 +152,11 @@ for k in range(1, 33+1, 2):
     KNN_Model.fit(X_train, y_train)
 
     y_predict_valid = KNN_Model.predict(X_valid)
+    y_predict_train = KNN_Model.predict(X_train)
+    y_predict_test = KNN_Model.predict(X_test)
     acc_valid = accuracy_score(y_valid, y_predict_valid)
+    acc_train = accuracy_score(y_train, y_predict_train)
+    acc_test = accuracy_score(y_test, y_predict_test)
 
     if acc_valid > best_accuracy:
         best_accuracy = acc_valid
@@ -160,7 +185,7 @@ print('Accuracy on Validation set:', acc_valid)
 print('Accuracy on Test set:', acc_test)
 
 
-# In[9]:
+# In[35]:
 
 
 #Decision Tree
@@ -179,7 +204,7 @@ plt.title("Decision tree trained on all features")
 plt.show()
 
 
-# In[10]:
+# In[36]:
 
 
 # tunning tree depth
@@ -217,7 +242,7 @@ plt.title(f"Decision tree trained on d = {best_d}")
 plt.show()
 
 
-# In[11]:
+# In[37]:
 
 
 X_train, X_valid, y_train, y_valid = train_test_split(X_standardized,y, train_size=0.75, random_state = 0)
@@ -233,7 +258,7 @@ cvs = cross_val_score(svm, X_standardized, y, cv = 10)
 print("default SVC cvs:",cvs)
 
 
-# In[12]:
+# In[38]:
 
 
 # Define hyperparameters to try
@@ -272,7 +297,7 @@ print(f"\nTest Set Accuracy with Best Hyperparameters: {test_accuracy}")
 print("Best Hyperparameters:", best_params)
 
 
-# In[13]:
+# In[39]:
 
 
 #1 vs. Rest
@@ -314,7 +339,7 @@ print(f"\nTest Set Accuracy with Best Hyperparameters: {test_accuracy}")
 print("Best Hyperparameters:", best_params)
 
 
-# In[14]:
+# In[47]:
 
 
 # 1 vs. 1
@@ -356,11 +381,12 @@ print("Best Hyperparameters:", best_params)
 
 
 
-# In[51]:
+# In[41]:
 
 
 #model persistance
 from tempfile import mkdtemp
+import joblib
 
 savedir = mkdtemp()
 
@@ -369,7 +395,7 @@ filename = os.path.join(savedir, 'OneVsRest.joblib')
 joblib.dump(final_clf, filename)  
 
 
-# In[52]:
+# In[42]:
 
 
 loaded_svc = joblib.load(filename)
@@ -378,14 +404,14 @@ test_accuracy = accuracy_score(y_test, y_pred_test)
 print(f"\nTest Set Accuracy with Best Hyperparameters: {test_accuracy}")
 
 
-# In[53]:
+# In[43]:
 
 
 filename1 = os.path.join(savedir, 'dts.joblib')
 joblib.dump(dts, filename1)  
 
 
-# In[54]:
+# In[44]:
 
 
 loaded_svc = joblib.load(filename1)
@@ -394,14 +420,14 @@ test_accuracy = accuracy_score(y_test, y_pred_test)
 print(f"\nTest Set Accuracy with Best Hyperparameters: {test_accuracy}")
 
 
-# In[55]:
+# In[48]:
 
 
 filename2 = os.path.join(savedir, 'OneVsOne.joblib')
 joblib.dump(final_clf1, filename2)  
 
 
-# In[56]:
+# In[49]:
 
 
 loaded_svc = joblib.load(filename2)
@@ -410,14 +436,14 @@ test_accuracy = accuracy_score(y_test, y_pred_test)
 print(f"\nTest Set Accuracy with Best Hyperparameters: {test_accuracy}")
 
 
-# In[57]:
+# In[50]:
 
 
 filename3 = os.path.join(savedir, 'SVM.joblib')
 joblib.dump(final_svm, filename3)  
 
 
-# In[58]:
+# In[51]:
 
 
 loaded_svc = joblib.load(filename3)
@@ -426,24 +452,18 @@ test_accuracy = accuracy_score(y_test, y_pred_test)
 print(f"\nTest Set Accuracy with Best Hyperparameters: {test_accuracy}")
 
 
-# In[59]:
+# In[52]:
 
 
 filename4 = os.path.join(savedir, 'KNN.joblib')
 joblib.dump(best_model, filename4)  
 
 
-# In[60]:
+# In[53]:
 
 
-loaded_svc = joblib.load(filename1)
+loaded_svc = joblib.load(filename4)
 y_pred_test = loaded_svc.predict(X_test)
 test_accuracy = accuracy_score(y_test, y_pred_test)
 print(f"\nTest Set Accuracy with Best Hyperparameters: {test_accuracy}")
-
-
-# In[ ]:
-
-
-
 
